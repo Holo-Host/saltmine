de52889c990753d02f7ed16db80613329bf9b10e
Paul B. Hartzog <paul.hartzog@holo.host>
2019-02-01 15:57:56 -0500

    completed, needs refactor

--- saltmine.js
+++ saltmine.js
@@ -72,6 +72,7 @@ async function handleRequest(request) {
     } else if (request.headers.get("Content-Type") !== 'application/x-www-form-urlencoded') {
         responseStatus = 415;
     } else {
+      // if method and headers are correct
       // get form data
       const data = await request.formData();
       // console.log(data)
@@ -84,7 +85,6 @@ async function handleRequest(request) {
         responseStatus = 400;
       } else {
         let salt = "";
-
         // from documentation
         // if incoming salt, then process it
         let sent_salt = data.get('salt')
@@ -96,40 +96,89 @@ async function handleRequest(request) {
 
           existing_salt = await SALTMINE.get(email);
           if(!existing_salt){
-            console.log("no salt");
-            // this uses the function above
-            //salt = await sha256(enc_email);
-            // Be sure these are strings
-            ip = ip.toString()
-            dna = dna.toString()
-            console.log("putting", ip + " " + dna)
-            IP_DNA_QUEUE.put(ip, dna)
-
+            console.log("no existing salt");
+            // gen token
+            let prng = await entropy(4);
+            console.log(prng);
+            // send email (later)
+            //
+            // put key,value into store
+            // Cloudflare will ignore duplicate keys
+            // we can check for duplicates later
+            //   so we can have nice error message
+            // At present there is no way to verify
+            // the ".put" command succeeds
+            // According to documentation
+            // eventual consistency < 10 seconds globally
+            let status = "pending";
+            let token = prng.toString();
+            let data_string = `{"status":"${status}", "token":"${token}", "salt":"${sent_salt}"}`;
+            console.log("putting",data_string);
+            SALTMINE.put(email, data_string);
+          } else {
+            // waitin for clarification here
+            console.log('data found',existing_salt);
           }
-
-
-
-
           salt = sent_salt;
-
         } else {
-          // url encode, this isn't strictly necessary
-          let enc_email = encodeURIComponent(email);
-          //console.log("url_encoded email: ", enc_email);
+          // if no salt sent in
+          // look for incoming token
+          let sent_token = data.get('token');
+          if(sent_token){
+            console.log("sent token", sent_token);
+            // verify token, change note
+            // return salt, else 401 error
+            let json = await SALTMINE.get(email, "json");
+            console.log(json);
+            if(json){
+              // verify token
+              if(sent_token == json.token){
+                console.log("token is good:", sent_token);
+                let status = "active";
+                let token = json.token;
+                salt = json.salt;
+                let data_string = `{"status":"${status}", "token":"${token}", "salt":"${salt}"}`;
+                console.log("putting",data_string);
+                SALTMINE.put(email, data_string);
 
-          // return salt if exists in KV store
-          // generate one if it doesn't
-          // try to get from KV store
-          salt = await SALTMINE.get(email);
-          if(!salt){
-            console.log("retrieved salt"); //,salt)
-            // this uses the function above
-            salt = await sha256(enc_email);
+              } else {
+                // bad token
+                console.log("token is bad:", sent_token);
+                responseStatus = 401;
+                responseInit = {
+                  status: responseStatus,
+                }
+                return new Response('Invalid token', responseInit);
+              }
+            } else {
+              // error out
+              responseStatus = 404;
+              responseInit = {
+                status: responseStatus,
+              }
+              return new Response('No data found', responseInit);
+            }
+          } else {
+            // if neither salt nor token incoming
+            // url encode, this isn't strictly necessary
+            let enc_email = encodeURIComponent(email);
+            //console.log("url_encoded email: ", enc_email);
+            // return salt if exists in KV store
+            // generate one if it doesn't
+            // try to get from KV store
+            let json = await SALTMINE.get(email, "json");
+            //console.log(json);
+            if(!salt){
+              console.log("generate salt");
+              // this uses the function above
+              salt = await sha256(enc_email);
+            } else {
+              console.log("retrieved salt", salt)
+              salt = obj.salt;
+            }
           }
         }
-        // we have everything so request is valid
-        // console.log("all request parameters VALID")
-
+        // final response
         responseObj.salt = salt;
         responseStatus = 200;
       }
