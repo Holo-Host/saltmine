b8947452d952ca1637e3dbc25e6984429f52a615
Willem Olding <willemolding@gmail.com>
2019-03-12 16:36:43 +1100

    adds a test directory which mocks and tests the service worker

---
+++ tests/mocks.js
@@ -0,0 +1,55 @@
+
+// mocks that are required for a cloudflare worker to run correctly in node
+
+module.exports = (() => {
+
+	global.addEventListener = (name, callback) => {}
+
+	var { Request, Response } = require('node-fetch');
+
+	Request.prototype['formData'] = function() {
+	  var obj
+	  try {
+	    var str = this.url.split('?')[1]
+	    obj = str.split("&").reduce(function(prev, curr, i, arr) {
+	      var p = curr.split("=");
+	      prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
+	      return prev;
+	    }, {});
+	  } catch (e) {
+	    console.log(e)
+	    obj = {}
+	  }
+	  return {get: i => obj[i]}
+	}
+
+	// set these on the global object for the worker
+	global.Request = Request;
+	global.Response = Response;
+	global.URL = require('url').URL;
+	global.TextEncoder = require('util').TextEncoder;
+
+
+	// a stubbed keyvalue store that exists for the life of the test
+	let kv = {}
+	global.SALTMINE = {
+	  get: k => { 
+	    console.log(`getting ${k} = ${kv[k]}`)
+	    return JSON.stringify(kv[k]) || JSON.stringify({})
+	  },
+	  put: (k, v) => {
+	    console.log(`storing ${k} = ${v}`)
+	    kv[k] = v
+	  }
+	}
+
+
+	// very stubbed crypto functions
+	global.crypto = {
+	  getRandomValues: arr => arr,
+	  subtle: {
+	    digest: (algo, buffer) => buffer
+	  }
+	};
+
+})();
---
+++ tests/test.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+
+require('./mocks');
+
+var rewire = require("rewire");
+var saltmine = rewire("../saltmine.js");
+
+var handleRequest = saltmine.__get__('handleRequest')
+
+
+/*====================================
+=            handleRequest            =
+====================================*/
+
+describe('handleRequest', () => {
+
+  it('should handle a GET request with no headers or body and return some entropy (64 bytes)', async () => {
+    const request = new Request('http://someurl', {
+      method: 'GET'
+    })
+
+    const response = await handleRequest(request);
+    console.log(response)
+    expect(response.status).to.equal(200);
+    expect(response.body.length).to.equal(64);
+    // expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
+  })
+
+
+  it('should handle a POST request with content-type form and no params', async () => {
+    const request = new Request('http://someurl', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+
+    const response = await handleRequest(request);
+    console.log(response)
+    expect(response.status).to.equal(500);
+    // expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
+  })
+
+
+  it('should handle a POST request with content-type form and an email only', async () => {
+    const request = new Request('http://someurl?email=asdfg', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+
+    const response = await handleRequest(request);
+    console.log(response)
+    expect(response.status).to.equal(200);
+    expect(response.body.length).to.equal(10); // this is just because of our mock hash implementation
+    // expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
+  })
+
+
+  it('should handle a POST request with content-type form and email and salt', async () => {
+    const request = new Request('http://someurl?email=asdfg&salt=12345', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+
+    const response = await handleRequest(request);
+    console.log(response)
+    expect(response.status).to.equal(200);
+    expect(response.body).to.equal('12345'); // return the salt back again
+    // expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
+  })
+})
+
+/*=====  End of handleRequest  ======*/
